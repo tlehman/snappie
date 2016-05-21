e353601403a4d816acfff6364b05cc2327c4ffd1
Tobi Lehman <mail@tobilehman.com>
2016-05-20 21:10:37 -0700

    onreadystatechanged and move cmds

--- static/js/main.js
+++ static/js/main.js
@@ -12,5 +12,8 @@ function stop() {
 
 function request(verb, path) {
     var xhr = new XMLHttpRequest("PUT", "http://robot.local:5000");
-    xhr.open(verb, path)
+    xhr.open(verb, path);
+    xhr.onreadystatechange = function() {
+        console.log(xhr.responseText)
+    }
 }
