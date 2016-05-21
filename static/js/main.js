3debda81855120b6f762590629a19cbde39e158b
Tobi Lehman <mail@tobilehman.com>
2016-05-20 21:41:00 -0700

    Send the data

--- static/js/main.js
+++ static/js/main.js
@@ -13,6 +13,7 @@ function stop() {
 function request(verb, path) {
     var xhr = new XMLHttpRequest("PUT", "http://robot.local:5000");
     xhr.open(verb, path);
+    xhr.send();
     xhr.onreadystatechange = function() {
         console.log(xhr.responseText)
     }
